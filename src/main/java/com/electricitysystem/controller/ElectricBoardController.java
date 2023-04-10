ea0034807614cf4d837eccc6e52d2e19bac7439e
anhtcogn <anhtcogn@gmail.com>
2023-04-10 09:25:24 +0700

    update electricBoard

--- src/main/java/com/electricitysystem/controller/ElectricBoardController.java
+++ src/main/java/com/electricitysystem/controller/ElectricBoardController.java
@@ -1,7 +1,45 @@
 package com.electricitysystem.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.electricitysystem.entity.ElectricBoardEntity;
+import com.electricitysystem.service.ElectricBoardService;
+import net.bytebuddy.implementation.auxiliary.AuxiliaryType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/board")
 public class ElectricBoardController {
+
+    @Autowired
+    private ElectricBoardService electricBoardService;
+
+    @PostMapping("/create")
+    public ElectricBoardEntity create(
+            @ModelAttribute ElectricBoardEntity electricBoard
+    ) {
+        return electricBoardService.create(electricBoard);
+    }
+    @PostMapping("/update")
+    public ElectricBoardEntity update(
+            @ModelAttribute ElectricBoardEntity electricBoard
+    ) {
+        return electricBoardService.update(electricBoard);
+    }
+
+    @GetMapping("/getOneById")
+    public ElectricBoardEntity getOneById(
+            @RequestParam int electricBoardId
+    ) {
+        return electricBoardService.getOneById(electricBoardId);
+    }
+
+    @GetMapping("/getAllByCustomerId")
+    public List<ElectricBoardEntity> getAllByCustomerId(
+            @RequestParam int customerId
+    ) {
+        return electricBoardService.getAllByCustomerId(customerId);
+    }
+
 }
