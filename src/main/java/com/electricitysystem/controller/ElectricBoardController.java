62b67c43620227a3faa04119c9c565e1003b0366
vah72 <hoangvananh987@Gmail.com>
2023-05-06 00:15:34 +0700

    change reponse data type

--- src/main/java/com/electricitysystem/controller/ElectricBoardController.java
+++ src/main/java/com/electricitysystem/controller/ElectricBoardController.java
@@ -4,6 +4,7 @@ import com.electricitysystem.entity.ElectricBoardEntity;
 import com.electricitysystem.service.ElectricBoardService;
 import net.bytebuddy.implementation.auxiliary.AuxiliaryType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,7 +19,8 @@ public class ElectricBoardController {
     @Autowired
     private ElectricBoardService electricBoardService;
 
-    @PostMapping("/create")
+    @PostMapping(value = "/create",consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE )
     public ResponseEntity<?> create(
             @RequestParam("file") MultipartFile files) throws IOException {
         electricBoardService.create(files);
